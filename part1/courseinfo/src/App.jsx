b5a1af386c141d596c40d08dae8b2d0283d28ed1
maybe0000 <saramarkic99@gmail.com>
2025-01-16 15:17:28 +0700

    Course info: 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,19 +4,23 @@ const Header = (props) => {
   )
 }
 
+const Part = ({part, exercise}) => {
+  return (
+    <div>
+      <p>
+        {part} {exercise}
+      </p>
+    </div>
+  )
+}
+
 const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
   return (
-    <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-    </>
+    <div>
+      <Part part={part1} exercise={exercises1}/>
+      <Part part={part2} exercise={exercises2}/>
+      <Part part={part3} exercise={exercises3}/>
+    </div>
   )
 }
 
