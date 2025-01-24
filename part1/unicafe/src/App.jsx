5e3e2eefbd25d04b731679aa6ebe51c395f856b0
maybe0000 <saramarkic99@gmail.com>
2025-01-24 18:33:25 +0700

    1.6: Unicafe step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,51 @@
 import { useState } from 'react'
 
+const Header = ({text}) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>{props.text}</button>
+  )
+}
+
+const Statistics = (props) => {
+  return (
+    <p>{props.text}: {props.count}</p>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  }
+
   return (
     <div>
-      code here
+      <Header text='Give feedback' />
+      <Button onClick={handleGoodClick} text='good'/>
+      <Button onClick={handleNeutralClick} text='neutral'/>
+      <Button onClick={handleBadClick} text='bad'/>
+      <Header text='Statistics'/>
+      <Statistics text='good' count={good}/>
+      <Statistics text='neutral' count={neutral}/>
+      <Statistics text='bad' count={bad}/>
     </div>
   )
 }
