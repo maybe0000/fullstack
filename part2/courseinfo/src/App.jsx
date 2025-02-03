3bd496481e03f3f3b94084693ca84cf38ecd8c1d
maybe0000 <saramarkic99@gmail.com>
2025-02-03 16:02:53 +0700

    2.4: Course information step 9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,5 +1,9 @@
+const Curriculum = ({ curriculum }) => {
+  return <h1>{curriculum}</h1>
+}
+
 const Header = ({ name }) => {
-  return <h1>{name}</h1>
+  return <h2>{name}</h2>
 }
 
 const Part = ({ part }) => {
@@ -20,41 +24,65 @@ const Total = ({ total }) => {
 }
 
 const Course = ({ course }) => {
-  return <div>
+  return <div className="course">
     <Header name={course.name} />
     <Content parts={course.parts} />
   </div>
 }
-
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <>
+      <Curriculum curriculum='Web development curriculum' />
+      <div id="courses">
+        {courses.map(course => <Course key={course.id} course={course} />)}
+      </div>
+    </>
+  )
 }
 
 export default App
