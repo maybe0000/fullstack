e379e94cd0f8cab1babb9df98ceef4094cc0a20f
maybe0000 <saramarkic99@gmail.com>
2025-07-29 15:06:05 +0200

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,7 +4,26 @@ const totalLikes = blogs => {
     return blogs.length === 0 ? 0 : blogs.reduce((sum, b) => sum + b.likes, 0)
 }
 
+const favoriteBlog = blogs => {
+    if (blogs.length === 0) {
+        return null;
+    }
+    if (blogs.length === 1) {
+        return blogs[0]
+    }
+    let blogIndex = 0
+    let count = 0
+    for (let i = 0; i < blogs.length; i++) {
+        if (blogs[i].likes > count) {
+            blogIndex = i
+            count = blogs[i].likes
+        }
+    }
+    return blogs[blogIndex]
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
