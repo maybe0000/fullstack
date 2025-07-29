90817e08555b19d0109ea2ee4c717ee6d76cb389
maybe0000 <saramarkic99@gmail.com>
2025-07-29 15:41:30 +0200

    4.6*: Helper Functions and Unit Tests, step 4 v2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,35 +8,19 @@ const favoriteBlog = blogs => {
     if (blogs.length === 0) {
         return null
     }
-    if (blogs.length === 1) {
-        return blogs[0]
-    }
-    let blogIndex = 0
-    let count = 0
-    for (let i = 0; i < blogs.length; i++) {
-        if (blogs[i].likes > count) {
-            blogIndex = i
-            count = blogs[i].likes
-        }
-    }
-    return blogs[blogIndex]
+
+    return blogs.reduce((max, i) => i.likes > max.likes ? i : max)
+
 }
 
 const mostBlogs = blogs => {
     if (blogs.length === 0) {
         return null
     }
-    let blogIndex = 0
     let blogMap = new Map()
-    for (let i = 0; i < blogs.length; i++) {
-        const author = blogs[i].author
 
-        if (blogMap.has(author)) {
-            blogMap.set(author, blogMap.get(author) + 1)
-        } else {
-            blogMap.set(author, 1)
-        }
-
+    for (const { author } of blogs) {
+        blogMap.set(author, (blogMap.get(author) || 0) + 1)
     }
 
     const maxBlogs = [...blogMap].reduce((max, blog) => blog[1] > max[1] ? blog : max)
