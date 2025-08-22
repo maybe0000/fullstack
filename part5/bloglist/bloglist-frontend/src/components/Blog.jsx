0e7fb1135e0a0d1b8d6c3b61211307346f55d49d
maybe0000 <saramarkic99@gmail.com>
2025-08-22 15:26:52 +0200

    5.7 Blog List Frontend, step 7

--- part5/bloglist/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [isClicked, setIsClicked] = useState(false)
+
+  const handleClick = () => {
+    setIsClicked(!isClicked)
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={handleClick}>{isClicked ? 'hide' : 'view'}</button>
+      {isClicked &&
+        <div>
+          {blog.url}
+          <br />
+          {blog.likes}
+          <br />
+          {blog.user.username}
+        </div>
+      }
+    </div>
+  )
+}
 
 export default Blog
